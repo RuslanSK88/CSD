b34ad98958c1f8361cc666c1fd230857d5397153
Руслан Сулейманов <ruslansk88@gmail.com>
2023-03-31 19:30:36 +0600

    Создаём новую миграцию с названием "Add Category and Post tables" Обновляем базу

--- CSD.Web/Models/Category.cs
+++ CSD.Web/Models/Category.cs
@@ -1,6 +1,24 @@
-﻿namespace CSD.Web.Models
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSD.Web.Models;
+
+[DisplayColumn("Категория")]
+public class Category
 {
-    public class Category
-    {
-    }
+    [Key]
+    public int Id { get; set; }
+
+    [Display(Name = "Имя")]
+    [Required]
+    public string? Name { get; set; }
+
+    [Display(Name = "Родительския категория")]
+    [Required]
+    public int ParentId { get; set; } = 0;
+
+    public List<Post>? Posts { get; set; }
 }
+
+
