bb188ed8e375ca7da6c2a93a657276f0e4a3c19c
Руслан Сулейманов <ruslansk88@gmail.com>
2023-03-31 20:54:41 +0600

    Редактирование категории

--- CSD.Web/Controllers/CategoriesController.cs
+++ CSD.Web/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using CSD.Web.Data;
 using CSD.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CSD.Web.Controllers;
 
@@ -36,5 +37,55 @@ public class CategoriesController : Controller
         return View(category);
     }
 
+    // GET: Categories/Create
+    public IActionResult Create()
+    {
+        ViewBag.Categories = new SelectList(_db.Categories.Where(x => x.ParentId == 0), "Id", "Name");
+        return View();
+    }
+
+    // POST: Categories/Create
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        ViewBag.Categories = new SelectList(_db.Categories.Where(x => x.ParentId == 0), "Id", "Name");
+        return View(category);
+    }
+
+    // GET: Categories/Edit/5
+    public ActionResult Edit(int id)
+    {
+        var category = _db.Categories.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        ViewBag.Categories = new SelectList(_db.Categories.Where(x => x.ParentId == 0), "Id", "Name");
+        return View(category);
+    }
+
+    // POST: Categories/Edit/5
+    [HttpPost]
+    public IActionResult Edit(int id, Category category)
+    {
+        if (id != category.Id)
+        {
+            return NotFound();
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Update(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        ViewBag.Categories = new SelectList(_db.Categories.Where(x => x.ParentId == 0), "Id", "Name");
+        return View(category);
+    }
 
 }
