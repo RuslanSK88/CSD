387adad7841f45d43fa3e91d5843f04918911193
Руслан Сулейманов <ruslansk88@gmail.com>
2023-03-31 19:28:07 +0600

    Added Category model

--- CSD.Web/Data/CSDContext.cs
+++ CSD.Web/Data/CSDContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CSD.Web.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSD.Web.Data;
 
@@ -8,5 +9,6 @@ public class CSDContext : DbContext
     {
 
     }
+    public DbSet<Category> Categories { get; set; }
 }
 
